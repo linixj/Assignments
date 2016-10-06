021e0d36e5b4a05006e5df8cf967ac05f4c6d6c5
linixj <nikkileenee@gmail.com>
2016-10-06 00:52:03 -0700

    w4

---
+++ Assignments/src/W4Assignment/Driver.java
@@ -0,0 +1,24 @@
+package W4Assignment;
+
+public class Driver {
+   private String name;
+   private double gasGasLevel;
+   
+   public Driver(String name){
+	   this.name = name;
+   }
+   
+   public void addGas(double amount){
+	   GasTank tank = new GasTank();
+	   tank.addGas(amount);
+   }
+   
+   public void getGasLevel(){
+	   GasTank tank = new GasTank();
+	   tank.getGasLevel();
+   }
+   
+   public void driveCar(){
+	   System.out.println("Car is running");
+   }
+}
